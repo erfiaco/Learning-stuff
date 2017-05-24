d56b0c1c1209584b41aa915f06340f14c675fe43
Javier <erfiaco@gmail.com>
2017-05-24 22:51:37 +0200

    new stuff

--- First_function.R
+++ First_function.R
@@ -1,25 +1,34 @@
-Df<-data.frame(Code1=c("A", "A", "B", "B", "B"), Code2=c("A", "A", "B", "C", "D"), Ok_email=c("Yes","No",NA,"Yes","Unknown"), stringsAsFactors = F)
+Df<-data.frame(Code1=c("A", "A", "B", "B", "B"), Country=c("Austria", "Germany", "Italy", "Italy", "Italy"), Code2=c("A", "A", "B", "C", "D"), Ok_email=c("Yes","No",NA,"Yes","Unknown"), stringsAsFactors = F)
 
-F_c<-function(x,y){
+F_c<-function(Country,x,y){
+  #attach(Dataframe)
   tab<-table(x,y, exclude=FALSE)
   nr<-nrow(tab)
   a<-numeric(nr)
+  db<-cbind(x,y)
+  email_country<-data.frame(cbind(x,Country), stringsAsFactors = FALSE)
+  library(plyr)
+  
+  email_country<-unique(email_country)
   
     for (i in 1:nr){
       a[i]<-sum(tab[i,])
     }
   
-  tab<-cbind(tab,Total=a)
+  tab<-cbind(x=rownames(tab), tab, Total=a)
+  merge.data.frame(x=tab,y=email_country, by = "x")
   
-  tab1<-tab[tab[,"Total"]>1,,drop=F]
-  nr<-nrow(tab1)
+  #tab1<-tab[tab[,"Total"]>1,,drop=F]
+  #nr<-nrow(tab1)
   Correct<-character(length = nr)
-  
+  tabf<-data.frame(cbind(tab,Correct), stringsAsFactors = FALSE)
+
     for (i in 1:nr){
-      Correct[i]<-if (tab1[i,"No"]>0) {"No"} else if (tab1[i,"Yes"]>0) {"Yes"} else {"Unknown"}
+      #Correct[i]<-if (tab1[i,"No"]>0) {"No"} else if (tab1[i,"Yes"]>0) {"Yes"} else {"Unknown"}
+      tabf$Correct[i]<-if (tab[i,"No"]>0) {"No"} else if (tab[i,"Yes"]>0) {"Yes"} else {"Unknown"}
     }
   
-  
-  tab1<-data.frame(cbind(tab1,Correct), stringsAsFactors = F)
-  tab1
+  #check<-join(bd, tabf, by="x")
+  #check
+  tabf
 }
